6ed5f956c53f8a66510a2da1e1656758456dff21
bpche <41422540+bpche@users.noreply.github.com>
2018-09-23 18:07:31 +0700

    Update Comparebutton.py

--- Comparebutton.py
+++ Comparebutton.py
@@ -6,8 +6,8 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Firefox()
 browser.get('http://automationpractice.com/index.php')
 
-Button_shirt = driver.find_elements_by_link_text('T-Shirt')
+Button_shirt = browser.find_elements_by_link_text('T-Shirt')
 Button_shirt.click()
 
-Button_compare = driver.find_element_by_css_selector('p.compare')
+Button_compare = browser.find_element_by_css_selector('p.compare')
 Button_compare.click()
