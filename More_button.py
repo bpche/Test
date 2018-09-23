49a7b64e89f71c26c2a3e2d30f1240c6d482d702
bpche <41422540+bpche@users.noreply.github.com>
2018-09-23 18:07:04 +0700

    Update More_button.py

--- More_button.py
+++ More_button.py
@@ -7,6 +7,6 @@ browser = webdriver.Firefox()
 browser.get('http://automationpractice.com/index.php')
 
 move(570, 826)
-more_button = driver.find_element_by_xpath('xpath=//ul[@id='homefeatured']/li[2]/div/div[2]/div[2]/a[2]/span']
+more_button = browser.find_element_by_xpath('xpath=//ul[@id='homefeatured']/li[2]/div/div[2]/div[2]/a[2]/span']
 more_button.click()
 
