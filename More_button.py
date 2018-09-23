52dedcaf1d2469d2620f517c7a359c08b7e52fe8
bpche <41422540+bpche@users.noreply.github.com>
2018-09-23 17:51:47 +0700

    More_Button
    More

---
+++ More_button.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+
+
+browser = webdriver.Firefox()
+browser.get('http://automationpractice.com/index.php')
+
+move(570, 826)
+more_button = driver.find_element_by_xpath('xpath=//ul[@id='homefeatured']/li[2]/div/div[2]/div[2]/a[2]/span']
+more_button.click()
+
